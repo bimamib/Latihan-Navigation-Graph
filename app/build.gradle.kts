7bc85a850f94ce48b3c43a34630384d36d9e84bf
bimamib <bimaprast1601@gmail.com>
2023-09-08 22:42:40 +0700

    Mengubah targetSDK 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.bima.mynavigation"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
